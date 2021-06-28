20879ca0e27a41ce0a3a1e4b8a6de65fa9be84a4
Vlad <vlad.shurigin2014@gmail.com>
2021-06-28 22:42:37 +0300

    Space Community update

--- SpaceGame/src/Screens/CompanyLogo.gd
+++ SpaceGame/src/Screens/CompanyLogo.gd
@@ -1,11 +1,6 @@
 extends Node
 
-export(String, FILE) var next_scene_path: = ""
-
 
 func _unhandled_input(event):
-	get_tree().change_scene(next_scene_path)
-
-
-func _get_configuration_warning() -> String:
-	return "next_scene path must not be empty!" if next_scene_path == "" else ""
+	get_tree().paused = false
+	get_tree().change_scene("res://src/Screens/MainScreen.tscn")
